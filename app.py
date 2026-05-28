e2908b26c078295d7ba3919ec998fa3c813110e6
ArthurNunes0710 <arthun1007@gmail.com>
2026-05-28 18:34:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template , url_for , request , redirect , session
+from flask import Flask , render_template ,  request , redirect 
 import requests
 import json
 from urllib.parse import urlparse
